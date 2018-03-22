ede180a3ac6ec643b8cb69a225af66552866a524
barthclem <barthclem@gmail.com>
2018-03-22 09:49:47 +0100

    initial commit

---
+++ public/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function() {
+
+  var socket = io();
+  var note = document.getElementById('note');
+  var board = document.getElementById('board');
+  var navBoard = document.getElementById('');
+
+  function sendNote(){
+    console.log('write something now');
+    socket.emit('master-write', {
+      note: board.value
+    });
+  }
+
+  socket.on('write-down', (data) => {
+    note.innerText = `${data.note}`;
+  });
+
+
+
+
+})();
